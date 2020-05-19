137a0483f7cc7eb0c77a2648a83381c990cc955a
Tilltheman92 <63450561+Tilltheman92@users.noreply.github.com>
2020-05-19 22:32:32 +0200

    Add files via upload

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-3.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-3.py
@@ -11,7 +11,30 @@ For example, if you are given the number 15, there are 3 possibilities to compos
 The following function shows one way to solve the problem but the code is not ideal or efficient.
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
+#changed capital X to n:
+#defined all variables on top of the function
+#If i make 2 functions out of int it says that solutions is an object and i cant iterate through it
+#Cant get rid of the nested for loop
 
+def my_function(n):
+    solutions = []
+    m= 0
+    for x in range(5, n):
+        for y in range(4, n):
+            for z in range(3, n):
+                if (x*x==y*y+z*z):
+                    solutions.append([x, y, z])
+   
+    for solution in solutions:
+        if m < max(solution):
+            m = max(solution)
+    return m
+
+n = input("What is the maximal length of the triangle side? Enter a number: ")
+
+"""OG CODE
+
+print("The longest side possible is " + str(my_function(int(n))))
 def my_function(X):
     solutions = []
     for x in range(5, X):
@@ -28,3 +51,4 @@ def my_function(X):
 X = input("What is the maximal length of the triangle side? Enter a number: ")
 
 print("The longest side possible is " + str(my_function(int(X))))
+"""
